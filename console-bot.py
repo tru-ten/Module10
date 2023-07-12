5389a27926ea212b9606061c54aae6fdda12f5ad
tru-ten <74614544+tru-ten@users.noreply.github.com>
2023-07-12 23:02:28 +0300

    Update console-bot.py

--- console-bot.py
+++ console-bot.py
@@ -1,4 +1,54 @@
-contact_book = {}
+from collections import UserDict
+
+class Field:
+    def __init__(self, value) -> None:
+        self.value = value
+
+class Name(Field):
+    ...
+
+class Phone(Field):
+    ...
+
+class Record:
+    def __init__(self, name, phone=None) -> None:
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone.value)
+
+    def add_phone(self, phone):
+        self.phones.append(phone.value)
+
+    def delete_phone(self, phone):
+        try:
+            self.phones.remove(phone.value)
+        except:
+            print(f'Phone {phone.value} is not listed')
+
+    def change_phone(self, OldPhone, NewPhone):
+        try:
+            index_phone = self.phones.index(OldPhone.value)
+            self.phones.pop(index_phone)
+            self.phones.insert(index_phone, NewPhone.value)
+        except ValueError:
+            print(f'Phone {OldPhone.value} is not listed')
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record.phones
+    
+    def delete_record(self, record):
+        try:
+            self.data.pop(record.name.value)
+        except KeyError:
+            print(f'Record {record} not found')
+
+    def search_record(self, record):
+        if record.name.value in self.data.keys():
+            return 'Record found'
+
+contact_book = AddressBook()
 
 def error_handler(func):
     def inner(*args):
@@ -24,16 +74,22 @@ def exit(args):
 
 @error_handler
 def add_user(args):
-    name, phone = args
-    contact_book[name] = phone
-    return f'User {name} added!'
+    name = Name(args[0])
+    phone = Phone(args[1])
+    rec = Record(name, phone)
+    contact_book.add_record(rec)
+    return f'User {name.value} added!'
 
 @error_handler
 def change_phone(args):
-    name, phone = args
-    old_phone = contact_book[name]
-    contact_book[name] = phone
-    return f'{name} now has a phone: {phone}\nOld number: {old_phone}'
+    name = Name(args[0])
+    phone = Phone(args[1])
+    old_phone = Phone(contact_book[name.value])
+    rec = Record(name, old_phone)
+    contact_book.delete_record(rec)
+    rec.change_phone(old_phone, phone)
+    contact_book.add_record(rec)
+    return f'{name.value} now has a phone: {phone.value}\nOld number: {old_phone.value}'
 
 def show_all(args):
     if len(contact_book)>0:
@@ -45,9 +101,9 @@ def show_all(args):
 
 @error_handler
 def show_phone(args):
-    name = args[0]
-    phone = contact_book[name]
-    return f'Phone: {phone}'
+    name = Name(args[0])
+    phone = Phone(contact_book[name.value])
+    return f'Phone: {phone.value}'
 
 HANDLERS = {
     'hello': hello_user,
